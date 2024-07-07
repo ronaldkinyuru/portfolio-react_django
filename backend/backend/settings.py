d0b6e02136810dba10580af1842adb4e5e9f95ee
ronaldkinyuru <ronaldkinyuru@gmail.com>
2024-07-08 00:49:25 +0300

    python3 manage.py runserver worked

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'api.User'
+#AUTH_USER_MODEL = 'api.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
